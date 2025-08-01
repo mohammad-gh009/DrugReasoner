7b526455355275f232bf08b4f7250f1506143987
Moreza <75425392+mohammad-gh009@users.noreply.github.com>
2025-08-01 17:53:17 +0330

    Delete codes-arxive directory

--- codes-arxive/download_llama3_2.py
+++
@@ -1,3 +0,0 @@
-
-from transformers import AutoTokenizer, AutoModelForCausalLM
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.2-1B", cache_dir="/home/u111169/wrkdir/mgh-project/models")
